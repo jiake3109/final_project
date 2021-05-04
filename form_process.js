c8e69747946242d752877f417288e7fa70a0d9e1
jiake3109 <78673932+jiake3109@users.noreply.github.com>
2021-05-03 22:46:50 -0400

    Add files via upload

--- form_process.js
+++ form_process.js
@@ -147,7 +147,7 @@ app.post('/process_post', urlencodedParser, function (req, res) {
                  var index = Math.floor(Math.random() * items.length);
                  console.log("The artwork name is: " + items[index].title + ".<br>" + "The artwork's url is:" + items[index].image_url + ".");
                  res.write("<div style='text-align:center;'><img src='" + items[index].image_url + "' style='height:400px; width:300px;'></div><br>");
-                 res.write("<div style='text-align:center;'>The artwork: <b>" + items[index].title + "</b> has been shared to <b>" + response['email'] + "</b><br><br></div>");
+                 res.write("<div class='message' style='text-align:center;'>The artwork: <b>" + items[index].title + "</b> has been shared to <b>" + response['email'] + "</b><br><br></div>");
                }
                db.close();
                console.log(response);
@@ -176,7 +176,7 @@ app.post('/process_post', urlencodedParser, function (req, res) {
                      var index = Math.floor(Math.random() * items.length);
                      console.log("The artwork name is: " + items[index].title + ".<br>" + "The artwork's url is:" + items[index].image_url + ".<br><br>");
                      res.write("<br><br><div style='text-align:center;'><img src='" + items[index].image_url + "' style='height:400px; width:300px;'></div><br>");
-                     res.write("<div style='text-align:center;'>The artwork: <b>" + items[index].title + "</b> has been shared to <b>" + response['email'] + "</b><br><br></div>");
+                     res.write("<div class='message' style='text-align:center;'>The artwork: <b>" + items[index].title + "</b> has been shared to <b>" + response['email'] + "</b><br><br></div>");
                  }
                }
                db.close();
